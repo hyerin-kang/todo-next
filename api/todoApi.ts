c6539836c381fcc41e4ab999a0a32e3fa9d18f6c
hyerin-kang <hkang0420@gmail.com>
2025-05-15 16:22:16 +0900

    수정

--- api/todoApi.ts
+++ api/todoApi.ts
@@ -1,22 +1,21 @@
-// apis/todoApi.ts
 export interface Todo {
   id: string;
   title: string;
   completed: boolean;
 }
 
-const BASE_URL = "https://grape-ethereal-patient.glitch.me";
+const BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL as string;
 
 //일기
 export const fetchTodos = async (): Promise<Todo[]> => {
-  const res = await fetch(BASE_URL);
+  const res = await fetch(`${BASE_URL}/todos`);
   if (!res.ok) throw new Error("투두 데이터 불러오기 실패");
   return res.json();
 };
 
 //추가
 export const addTodo = async (newTodo: Todo): Promise<void> => {
-  const res = await fetch(BASE_URL, {
+  const res = await fetch(`${BASE_URL}/todos`, {
     method: "POST",
     headers: { "Content-Type": "application/json" },
     body: JSON.stringify(newTodo),
@@ -27,7 +26,7 @@ export const addTodo = async (newTodo: Todo): Promise<void> => {
 
 //수정
 export const updateTodo = async (updateTodo: Todo) => {
-  const res = await fetch(`${BASE_URL}/${updateTodo.id}`, {
+  const res = await fetch(`${BASE_URL}/todos/${updateTodo.id}`, {
     method: "PATCH",
     headers: { "Content-Type": "application/json" },
     body: JSON.stringify(updateTodo),
@@ -38,7 +37,7 @@ export const updateTodo = async (updateTodo: Todo) => {
 
 //삭제
 export const deleteTodo = async (id: string) => {
-  const res = await fetch(`${BASE_URL}/${id}`, {
+  const res = await fetch(`${BASE_URL}/todos/${id}`, {
     method: "DELETE",
   });
   if (!res.ok) throw new Error("할일 삭제 실패");
