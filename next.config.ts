99b20b4cc8c12431c2f5e105442cbd643a1aa409
hyerin-kang <hkang0420@gmail.com>
2025-05-15 17:38:20 +0900

    수정

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,11 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  reactStrictMode: true,
+  env: {
+    // json-server URL 설정
+    API_URL: process.env.API_URL || "http://localhost:3000",
+  },
 };
 
 export default nextConfig;
